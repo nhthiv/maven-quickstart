74a22e4836d30d67ddc3f686214ddd8cbbdcc84c
Harithivakar <harit19963@gmail.com>
2018-09-08 16:52:09 +0530

    Changing compiler plugin to latest

---
+++ src/main/java/nhthiv/test/training/Application.java
@@ -0,0 +1,28 @@
+package nhthiv.test.training;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+	
+	public void greet() {
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for(String greetings){
+			System.out.println("Inside Application");
+		}
+			
+	}
+    
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+	Application app = new Application();
+	app.greet();
+    }
+}
